cf622712b6293ef41e230f6c23a9f44bd4cf6f6b
Fae <fae@catscradle.us>
2025-02-16 13:54:23 +0000

    Moved components into the component folder

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -1,20 +1,10 @@
 import { useState } from "react";
+import CalculateWinner from "./CalculateWinner";
+import Square from "./Square";
 
 function Board({ xIsNext, squares, onPlay }) {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      if (squares[i]) console.log("Space occupied");
+    if (CalculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -24,6 +14,14 @@ function Board({ xIsNext, squares, onPlay }) {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
+  }
+
+  const winner = CalculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
@@ -46,34 +44,6 @@ function Board({ xIsNext, squares, onPlay }) {
       </div>
     </>
   );
-}
-
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 export default Board;
